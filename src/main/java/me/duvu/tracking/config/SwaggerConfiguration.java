21f6b09e80a8e397372fe9ad3f8a37215ff6db25
duvu <hoaivubk@gmail.com>
2020-02-27 12:49:12 +0700

    refactor code

--- src/main/java/me/duvu/tracking/config/SwaggerConfiguration.java
+++ src/main/java/me/duvu/tracking/config/SwaggerConfiguration.java
@@ -18,17 +18,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfiguration {
-//    @Bean
-//    public WebMvcConfigurerAdapter forwardToIndex() {
-//        return new WebMvcConfigurerAdapter() {
-//            @Override
-//            public void addViewControllers(ViewControllerRegistry registry) {
-//                registry.addViewController("/api-docs").setViewName(
-//                        "forward:/swagger-ui.html");
-//            }
-//        };
-//    }
-
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -51,11 +40,4 @@ public class SwaggerConfiguration {
                 .version("1.0")
                 .build();
     }
-
-//    private Predicate<String> apiPaths() {
-//        return Predicates.or(
-//                regex("/manage.*"),
-//                regex("/api.*"),
-//                regex("/api-docs.*"));
-//    }
 }
